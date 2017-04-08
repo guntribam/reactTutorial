2753178abbb3db47f3cd1a31b8e21c4577736bf1
gunter.amorim <gunter.amorim@gmail.com>
2017-04-08 13:08:27 -0300

    Primeiro Commit

---
+++ src/test/java/br/com/gramorim/ReactTutorialApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.gramorim;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReactTutorialApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
